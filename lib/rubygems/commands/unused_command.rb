298ff2bddd9434ac01ac57a317022d2e46f982f5
Chris Apolzon <apolzon@gmail.com>
2012-02-18 20:38:25 -0800

    found all unused gems

--- lib/rubygems/commands/unused_command.rb
+++ lib/rubygems/commands/unused_command.rb
@@ -41,6 +41,7 @@ class Gem::Commands::UnusedCommand < Gem::Command
     options[:branches].each do |branch|
       `git co #{branch}`
       bundle = ::Bundler::LockfileParser.new(File.read("Gemfile.lock"))
+      # this is omitting development dependencies
       @requirements.concat bundle.specs
     end
     @requirements.uniq! {|spec| "#{spec.name} #{spec.version}" }
@@ -49,7 +50,19 @@ class Gem::Commands::UnusedCommand < Gem::Command
       puts "#{spec.name} #{spec.version}"
     end
     # now we need to find all the gems in the gemset
-
+    @installed = []
+    # TODO: we need to skip any that are installed in the global gemset
+    Gem::Specification.find_all do |installed_spec|
+      @installed << installed_spec
+    end
+    @installed.uniq! {|spec| "#{spec.name} #{spec.version}" }
+    @unneeded = @installed.delete_if do |installed_spec|
+      @requirements.any? {|spec| spec.name == installed_spec.name && spec.version == installed_spec.version}
+    end
+    puts "====REMOVE THESE GEMS===="
+    @unneeded.each do |bad_gem|
+      puts "#{bad_gem.name} #{bad_gem.version}"
+    end
   end
 
   def remove_unused_gems
